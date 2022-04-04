ad54739342b1c671a7ec7c01951a7b7c44a6bc21
Movsar Makhmutov <melkoto@mail.ru>
2022-04-04 11:31:55 +0300

    Update index.js

--- exercise1/index.js
+++ exercise1/index.js
@@ -1,1 +1,10 @@
+function jsStyle() {
+  // your code here
+}
 
+let elem = document.querySelector('jsstyle')
+console.log(elem)
+
+elem.addEventListener('click', () => {
+  console.log('clicked click button')
+})
